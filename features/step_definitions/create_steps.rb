9c2d5b363de39cff914f8d0b183577d6499219b6
Pranav Moktali <pmoktali@enova.com>
2015-07-07 13:49:55 -0500

    fixing tests

--- features/step_definitions/create_steps.rb
+++ features/step_definitions/create_steps.rb
@@ -20,7 +20,7 @@ Then 'create a bomb' do
   step 'I click on create button'
 end
 
-Then 'show a notification "([^"]*)"' do |status|
+Then /^show a notification "([^"]*)"$/ do |status|
   fail 'error' unless page.body.include? status
 end
 
@@ -28,15 +28,15 @@ Given 'activation code input is empty' do
   step 'no bomb is booted/created'
 end
 
-Then 'set activation code to (\d+)' do
+Then /^set activation code to (\d+)$/ do |activation_code|
   step 'create a bomb'
 end
 
-When 'I input an activation code "([^"]*)"' do |activation_code|
+When /^I input an activation code "([^"]*)"$/ do |activation_code|
   @options['activation_code'] = activation_code
 end
 
-Then 'set the activation code to "([^"]*)"' do
+Then /^set the activation code to "([^"]*)"$/ do |activation_code|
   step 'create a bomb'
 end
 
@@ -44,10 +44,10 @@ Given 'deactivation code input is empty' do
   step 'no bomb is booted/created'
 end
 
-Then 'set deactivation code to "([^"]*)"' do
+Then /^set deactivation code to "([^"]*)"$/ do |deactivation_code|
   step 'create a bomb'
 end
 
-When 'I input a deactivation code "([^"]*)"' do |deactivation_code|
+When /^I input a deactivation code "([^"]*)"$/ do |deactivation_code|
   @options['deactivation_code'] = deactivation_code
 end
